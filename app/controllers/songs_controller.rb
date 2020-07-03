3717b8c07b6ea02220fc6a3b37524a183316c64b
nathan-stamand <nathansaint810@gmail.com>
2020-07-03 18:28:39 -0500

    added CRUD actions and private method #song_params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,21 @@
 class SongsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :author, :chords, :key, :tempo)
 end
