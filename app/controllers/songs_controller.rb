b172e494943fabe2b48e3012924db1f8e6d82d9d
nathan-stamand <nathansaint810@gmail.com>
2020-07-12 13:16:39 -0500

    defined #update and #destroy, added :chord_feeds to #song_params permitted params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,17 +10,25 @@ class SongsController < ApplicationController
   end
 
   def create
+    song = Song.create(song_params)
+    render json: SongSerializer.new(song)
   end
 
   def update
+    song = Song.find_by(params[:id])
+    song.update(song_params)
+    render json: SongSerializer.new(song)
   end
 
   def destroy
+    song = Song.find_by(params[:id])
+    song.destroy
+    render json: {id: song.id}
   end
 
   private
 
   def song_params
-    params.require(:song).permit(:title, :author, :chords, :key, :tempo)
+    params.require(:song).permit(:title, :custom_chords, :key, :tempo, :mode, :chord_feeds)
   end
 end
