38ee3398121e58b6ecd9ddde0a0cd0f37f536a43
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 23:59:01 -0500

    modified #create to also create 4 chord_feeds for the song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,11 +11,15 @@ class SongsController < ApplicationController
 
   def create
     song = Song.create(song_params)
+    song.chord_feeds.create(position: 1, chord_array: '')
+    song.chord_feeds.create(position: 2, chord_array: '')
+    song.chord_feeds.create(position: 3, chord_array: '')
+    song.chord_feeds.create(position: 4, chord_array: '')
     render json: SongSerializer.new(song)
   end
 
   def update
-    song = Song.find_by(params[:id])
+    song = Song.find_by(id: params[:id])
     song.update(song_params)
     render json: SongSerializer.new(song)
   end
