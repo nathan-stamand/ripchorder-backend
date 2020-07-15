c7eae1cbfeed50413334caf90dd1d8bb9dfa5e42
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 23:59:48 -0500

    added #create, #update, and private #chord_feed_params

--- app/controllers/chord_feeds_controller.rb
+++ app/controllers/chord_feeds_controller.rb
@@ -1,2 +1,19 @@
 class ChordFeedsController < ApplicationController
+  def create
+    chord_feed = ChordFeed.create(chord_feed_params)
+    render json: ChordFeedSerializer.new(chord_feed)
+  end
+
+  def update
+    chord_feed = ChordFeed.find_by(id: params[:id])
+    chord_feed.update(chord_feed_params)
+    chord_feed.save
+    render json: ChordFeedSerializer.new(chord_feed)
+  end
+  
+  private
+
+  def chord_feed_params
+    params.require(:chord_feed).permit(:position, :chord_array, :song_id)
+  end
 end
